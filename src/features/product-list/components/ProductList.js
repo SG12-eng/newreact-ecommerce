24a2cda948a4f8ae3980fb012480713cc9357677
Sagar Gupta <guptasagar694@gmail.com>
2024-10-23 12:42:17 +0530

    Checkout, ProductDetail Page added

--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -1,10 +1,10 @@
 import React, { useState, Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, incrementAsync, selectCount } from './productListSlice';
+import { increment, incrementAsync, selectCount } from '../productSlice';
 import { Dialog, Disclosure, Menu, Transition } from '@headlessui/react';
 import { XMarkIcon } from '@heroicons/react/24/outline';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
-
+import { Link } from 'react-router-dom';
 import {
   ChevronDownIcon,
   FunnelIcon,
@@ -69,7 +69,7 @@ const products = [
     name: 'Basic Tee',
     href: '#',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
@@ -79,7 +79,7 @@ const products = [
     name: 'Basic Tee',
     href: '#',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
@@ -89,7 +89,7 @@ const products = [
     name: 'Basic Tee',
     href: '#',
     imageSrc:
-      'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+      'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
     imageAlt: "Front of men's Basic Tee in black.",
     price: '$35',
     color: 'Black',
@@ -273,7 +273,6 @@ export default function ProductList() {
                 <span className="sr-only">View grid</span>
                 <Squares2X2Icon className="h-5 w-5" aria-hidden="true" />
               </button>
-              
               <button
                 type="button"
                 className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
@@ -359,6 +358,7 @@ export default function ProductList() {
                   <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
                     <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
                       {products.map((product) => (
+                        <Link to="/product-detail">
                         <div key={product.id} className="group relative">
                           <div className="min-h-80 aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
                             <img
@@ -387,6 +387,7 @@ export default function ProductList() {
                             </p>
                           </div>
                         </div>
+                        </Link>
                       ))}
                     </div>
                   </div>
